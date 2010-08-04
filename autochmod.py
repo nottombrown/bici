3df53e49cc00b1f970bc30da72345c8fd7c07460
Tom Brown <tombrown@zerogarden.(none)>
2010-08-04 00:24:11 +0000

    added a little script

---
+++ autochmod.py
@@ -0,0 +1,7 @@
+import os
+
+chmod_macro = "chmod 777 media/ db/ db/db"
+#this command will allow the user to execute the outfile with a ./outfile
+
+os.system(chmod_macro) 
+#execute the chmod macro
